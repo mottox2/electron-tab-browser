a69a04f546887c1c207f7a90b3c83e98e5a87e4c
Yuki Takemoto <mottox2@users.noreply.github.com>
2016-01-24 01:51:15 +0900

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,6 @@ var gulp = require('gulp');
 var webpack = require('gulp-webpack');
 var webpackConfig = require('./webpack.config.js');
 
-var browserSync = require('browser-sync').create();
-
-gulp.task('browsersync', function() {
-  browserSync.init({
-    proxy: "localhost:3000"
-  });
-
-  return gulp.watch("./*.js", ['reload'])
-})
-
-gulp.task('reload', function() {
-  browserSync.reload();
-});
-
 gulp.task('build', function() {
   return gulp.src('./app.js')
              .pipe(webpack(Object.assign({}, webpackConfig, {
@@ -24,6 +10,4 @@ gulp.task('build', function() {
              .pipe(gulp.dest('./dist/'));
 });
 
-// gulpとタスク名を指定しないと default が起動します。
-// gulp.task('default', ['build', 'browsersync']);
 gulp.task('default', ['build']);
